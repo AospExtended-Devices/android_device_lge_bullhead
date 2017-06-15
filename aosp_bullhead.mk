7e7e8a4145f0ecfa307bfc12867bab670402b417
Arseniy Graur <argraur12@yandex.ru>
2017-06-15 19:44:38 +0000

    aosp_bullhead.mk: Some changes

--- aosp_bullhead.mk
+++ aosp_bullhead.mk
@@ -26,15 +26,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_NAME := aosp_bullhead
 PRODUCT_DEVICE := bullhead
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on BullHead
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 5X
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 #PRODUCT_COPY_FILES += device/lge/bullhead/fstab.aosp_bullhead:root/fstab.bullhead
 
 $(call inherit-product, device/lge/bullhead/device.mk)
-$(call inherit-product-if-exists, vendor/lge/bullhead/bullhead-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/bullhead/device-vendor.mk)
 $(call inherit-product, vendor/aosp/common.mk)
 
 # Boot animation
