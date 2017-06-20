8bffb44a6bcf503367812a4845fd3655abcd9ea6
Arseniy Graur <argraur12@yandex.ru>
2017-06-20 08:17:42 +0000

    aosp_bullhead: Bring back build.prop overrides due to SafetyNet fail.

--- aosp_bullhead.mk
+++ aosp_bullhead.mk
@@ -44,3 +44,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_PACKAGE += \
   Launcher3
+
+PRODUCT_BUILD_PROP_OVERRIDES += \		
+    PRODUCT_NAME=bullhead \		
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.2/N2G47W/3938523:user/release-keys" \		
+    PRIVATE_BUILD_DESC="bullhead-user 7.1.2 N2G47W 3938523 release-keys"
+ 
